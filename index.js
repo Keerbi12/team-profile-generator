53ea502deb42d93022ea8e61239b1872396e8bbb
Keerbi Liu <kirby.liu12@gmail.com>
2021-10-03 21:50:46 +1100

    Added template for HTML creating, added Engineer, and intern constructors.

--- index.js
+++ index.js
@@ -1,59 +1,82 @@
 const inquirer = require("inquirer");
-const Manager = require
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+// team array
+const teamArr = [];
 
 // ========== Function that runs the questions related to manager =============
-// const manager = () => {
+const newManager = () => {
+    return inquirer
+    .prompt([
+      /* Pass your questions in here */
+      {
+          type: "input",
+          name: "name",
+          message: "What is the manager's name?",
+          validate: async (input) => {
+              if(input.trim('') === "") {
+                  return "Please input a name."
+              }
+              return true;
+          }
+      },
+      {
+          type: "input",
+          name: "id",
+          message: "What is the manager's Id?",
+          validate: async (input) => {
+              if(input.trim('') === "") {
+                  return "Please input an Id."
+              }
+              return true;
+          }
+      },
+      {
+          type: "input",
+          name: "email",
+          message: "What is the manager's Email?",
+          validate: async (input) => {
+              if(input.trim('') === "") {
+                  return "Please input an Email."
+              }
+              return true;
+          }
+      },
+      {
+          type: "input",
+          name: "github",
+          message: "What is the manager's GitHub username?",
+          validate: async (input) => {
+              if(input.trim('') === "") {
+                  return "Please input a GitHub username."
+              }
+              return true;
+          }
+      },
+    ])
+    .then((managerData) => {
+      // Use user feedback for... whatever!!
+      const { name, id, email} = managerData;
+      const manager = new Manager (name, id, email);
 
-// }
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-    {
-        type: "input",
-        name: "managerName",
-        message: "What is the manager's name?",
-        validate: async (input) => {
-            if(input.trim('') === "") {
-                return "Please input a name."
-            }
-            return true;
+      teamArr.push(manager);
+      console.log(manager);
+    })
+};
+
+newManager()
+
+// Function for creating HTML page via template.
+const writeFile = data => {
+    FileSystem.writeFile("./dist/index.html", data, err => {
+        if (err) {
+            console.log(err);
+            return;
+        // Success
+        } else {
+            console.log("New team HTML file created.")
         }
-    },
-    {
-        type: "input",
-        name: "managerId",
-        message: "What is the manager's Id?",
-        validate: async (input) => {
-            if(input.trim('') === "") {
-                return "Please input an Id."
-            }
-            return true;
-        }
-    },
-    {
-        type: "input",
-        name: "managerEmail",
-        message: "What is the manager's Email?",
-        validate: async (input) => {
-            if(input.trim('') === "") {
-                return "Please input an Email."
-            }
-            return true;
-        }
-    },
-    {
-        type: "input",
-        name: "managerGitHub",
-        message: "What is the manager's GitHub username?",
-        validate: async (input) => {
-            if(input.trim('') === "") {
-                return "Please input a GitHub username."
-            }
-            return true;
-        }
-    },
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-    console.log(answers)
-  })
+    });
+}
---
+++ src/template.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+const createHtml = files => {
+    let overallFile = 
+    `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>My Team Members</title>
+            <link rel="stylesheet" href="./style.css" type="text/css">
+        </head>
+        <body>
+            <!-- Page title -->
+            <header>
+                <p>My Team</p>
+                <p>A list of people who work at the company</p>
+            </header>
+            <!-- Staff section -->
+            <main>
+                <section>
+                    ${files}
+                </section>
+            </main>
+        </body>
+        </html> 
+    `
+};
+
+fs.writeFile("dist/index.html", overallFile, (err) => {
+    err ? console.log(err) : console.log('New HTML created.')
+});
+
+module.exports = createHtml;
